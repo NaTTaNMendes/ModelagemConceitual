74a0e343baa4bcefa3b76fdca1e1f87ca0a88170
Nattan <nattan078@gmail.com>
2022-02-14 16:46:04 -0300

    Repository e service para categoria

--- src/main/java/com/NaTTaNMendes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/NaTTaNMendes/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,25 @@
 package com.NaTTaNMendes.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.NaTTaNMendes.cursomc.domain.Categoria;
+import com.NaTTaNMendes.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@GetMapping 
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> list = new ArrayList<>();
-		
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}") 
+	public ResponseEntity<?> encontrar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
